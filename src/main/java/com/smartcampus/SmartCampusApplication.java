31355fb9d857c37d9c4f5297038d656955074254
gahana-gautam <gahana.gautam17@gmail.com>
2026-04-26 17:00:48 +0100

    Initial commit: Smart Campus JAX-RS API starter

---
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+/**
+ *
+ * @author Asus
+ */
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+    
+}
